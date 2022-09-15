feee1e0edb297bd6d1b27ebe294a407274e70227
Skorpiyo <skorpio277@gmail,com>
2022-09-15 16:18:02 +0300

    Update readme.md

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,8 +2,7 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        String text = "I`m updated\n"+
-                "can be compiled. And can write to console.";
+        String text = "I'm updated\ncan be compiled. And can write to console";
 
         System.out.println(text);
     }
